045fdcab8c11b362742e54264242f0c2d3c669c4
Kiril Chuchev <54711535+KirilChuchev@users.noreply.github.com>
2019-09-16 11:38:58 +0300

    Add files via upload

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+	using System.Numerics;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			uint number = uint.Parse(Console.ReadLine());
+
+			uint snowballSnow = 0;
+			uint snowballSnowMax = 0;
+			uint snowballTime = 0;
+			uint snowballTimeMax = 0;
+			uint snowballQuality = 0;
+			uint snowballQualityMax = 0;
+
+
+
+
+			BigInteger snowballValue = 0;
+			BigInteger snowballValueCurrent = 0;
+			BigInteger snowballValueMax = 0;
+
+
+
+			for (uint i = 1; i <= number; i++)
+			{
+				snowballSnow = uint.Parse(Console.ReadLine());
+				snowballTime = uint.Parse(Console.ReadLine());
+				snowballQuality = uint.Parse(Console.ReadLine());
+
+				snowballValueCurrent = (BigInteger)Math.Pow((snowballSnow / snowballTime), snowballQuality);
+				snowballValue = snowballValueCurrent;
+
+				
+				if (snowballValue >= snowballValueMax)
+				{
+					snowballValueMax = snowballValue;
+					snowballSnowMax = snowballSnow;
+					snowballTimeMax = snowballTime;
+					snowballQualityMax = snowballQuality;
+				}
+			}
+
+
+			if (number > 0)
+			{
+				Console.WriteLine($"{snowballSnowMax} : {snowballTimeMax} = {snowballValueMax} ({snowballQualityMax})");
+
+			}
+
+			
+		}
+	}
+}
---
+++ test/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace test
+{
+	using System.Numerics;
+
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			
+			int n = 100;
+			BigInteger number = n;
+
+			for (int i = 1; i <= 1000; i++)
+			{
+				number *= n;
+			}
+
+			Console.WriteLine(number);
+			Console.WriteLine(Math.Pow(0,5));
+
+			Console.WriteLine(n / 0);
+		}
+	}
+}
